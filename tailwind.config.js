c93400d93b0130510e023aa790796e56ccb8b085
Shamimur Rahaman <shamimurrahaman440@gmail.com>
2024-01-24 16:48:26 +0600

    added navbar and main

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: ["./src/**/*.{html,js}"],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
+module.exports = {
+    theme: {
+      extend: {
+        rotate: {
+          '17': '17deg',
+        }
+      }
+    }
+  }
